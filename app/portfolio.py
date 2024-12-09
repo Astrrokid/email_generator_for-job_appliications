89a649f502dc752584fe522f4cc2a91f864271da
Astrrokid <kidstarro22@gmail.com>
2024-12-09 15:37:40 +0100

    updated requirements.txt

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import chromadb
-import sqlite3
 import uuid
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 class Portfolio:
     def __init__(self, file_path='app/resource/x.csv'):
